2b01551abc855513448dd14eb377debad6da834e
mikko.orasuo@gmail.com <mikko.orasuo@gmail.com@e32d87d7-132d-e7c1-836b-198017e8af57>
2013-02-22 09:26:25 +0000

    CarPool

--- src/iut/android/iut_carpool/CarPool.java
+++ src/iut/android/iut_carpool/CarPool.java
@@ -1,17 +1,38 @@
 package iut.android.iut_carpool;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class CarPool extends Activity {
+public class CarPool extends Activity implements OnClickListener{
 
+	
+	Button jeCherche, jePropose;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_pool);
+        setContentView(R.layout.launch);
+    
+        jeCherche = (Button) findViewById(R.id.jeCherche); 
+        jeCherche.setOnClickListener(this);
+        
+        jePropose = (Button) findViewById(R.id.jePropose); 
+        jePropose.setOnClickListener(this);
+        
+        
+        
+    
+    
+    
     }
 
     @Override
@@ -19,6 +40,28 @@ public class CarPool extends Activity {
         getMenuInflater().inflate(R.menu.activity_car_pool, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+		
+		case R.id.jeCherche:
+			Log.i("Car Pool", "Je cherche");
+			
+			Intent jeChe = new Intent(this, JeCherche.class);
+	        startActivity(jeChe);
+			break;
+			
+		case R.id.jePropose:
+			Log.i("Car Pool", "Je propose");
+			
+			Intent jePro = new Intent(this, JePropose.class);
+	        startActivity(jePro); 
+	    	break;
+		
+		}
+		
+	}
 
     
 }
